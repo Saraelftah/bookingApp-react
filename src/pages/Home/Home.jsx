9d7e9d51e625c71d29bda762451447814714fdad
Sara <asara9086@gmail.com>
2025-07-31 02:41:14 +0300

    fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,9 @@
-// import { useEffect, useState } from "react";
 import Offers from "../../components/Offers/Offers";
 import RecommHotels from "../../components/RecommHotels/RecommHotels";
-import "./Home.module.css";
 
 
 function Home() {
-  // const [counter, setCounter] = useState(0);
 
-  // useEffect(() => {}, []);
 
   return (
     <>
